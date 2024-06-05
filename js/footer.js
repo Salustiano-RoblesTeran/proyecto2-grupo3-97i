06ec0a12da1718b66593658cc0e391cd68b4704c
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-06-04 22:34:20 -0300

    Hacemos la landing page

--- js/footer.js
+++ js/footer.js
@@ -5,8 +5,8 @@ function Footer() {
     section.innerHTML = `
         <article class="row" id="cuerpoFooter">
             <aside class="col-xl-3 col-sm-3 col-md-3">
-              <p class="h5">Nuestra Parte</p>
-              <ul>
+              <p class="h5 text-light m-2">Nuestra Parte</p>
+              <ul class="text-light m-2">
                 <li>Direccion: Av. General Paz 576,
                 Piso 9, oficina 2
                 San Miguel de Tucumán, Argentina</li>
@@ -15,45 +15,45 @@ function Footer() {
               </ul>
             </aside>
             <aside class="col-xl-3 col-sm-3 col-md-3">
-              <p class="h5">Navegacion</p>
+              <p class="h5 text-light m-2">Navegacion</p>
               <ul>
-                <li><a href="">Quienes Somos</a></li>
-                <li><a href="">Inicio</a></li>
-                <li><a href="">Contactanos</a></li>
-                <li><a href="">Nuestra Historia</a></li>
+                <li><a class=" text-light m-2" href="">Quienes Somos</a></li>
+                <li><a class=" text-light m-2" href="">Inicio</a></li>
+                <li><a class=" text-light m-2" href="">Contactanos</a></li>
+                <li><a class=" text-light m-2" href="">Nuestra Historia</a></li>
               </ul>
             </aside>
             <aside class="col-xl-3 col-sm-3 col-md-3">
-              <p class="h5">Legal</p>
+              <p class="h5 text-light m-2">Legal</p>
               <ul>
-                <li><a href="./page/error404.html">Terminos y condiciones</a></li>
-                <li><a href="./page/error404.html">Politica de privacidad</a></li>
+                <li><a class="text-light m-2" href="./page/error404.html">Terminos y condiciones</a></li>
+                <li><a class="text-light m-2" href="./page/error404.html">Politica de privacidad</a></li>
                 <li>
-                  <a href="./page/error404.html">Politica de derecho de autor</a>
+                  <a class="text-light m-2" href="./page/error404.html">Politica de derecho de autor</a>
                 </li>
-                <li><a href="./page/error404.html">Descargar App Movil</a></li>
+                <li><a class="text-light m-2" href="./page/error404.html">Descargar App Movil</a></li>
               </ul>
             </aside>
             <aside class="col-xl-3 col-sm-3 col-md-3">
-              <p class="h5">Redes Sociales</p>
+              <p class="h5 text-light m-2">Redes Sociales</p>
               <ul>
                 <li>
-                  <a href="./page/error404.html"><i class="bi bi-facebook"></i> Facebook</a>
+                  <a  class="text-light m-2"href="./page/error404.html"><i class="bi bi-facebook"></i> Facebook</a>
                 </li>
                 <li>
-                  <a href="./page/error404.html"><i class="bi bi-twitter"></i> Twitter</a>
+                  <a class="text-light m-2" href="./page/error404.html"><i class="bi bi-twitter"></i> Twitter</a>
                 </li>
                 <li>
-                  <a href="./page/error404.html"><i class="bi bi-instagram"></i> Instagram</a>
+                  <a class="text-light m-2" href="./page/error404.html"><i class="bi bi-instagram"></i> Instagram</a>
                 </li>
                 <li>
-                  <a href="./page/error404.html"><i class="bi bi-linkedin"></i> LinkedIn</a>
+                  <a class="text-light m-2" href="./page/error404.html"><i class="bi bi-linkedin"></i> LinkedIn</a>
                 </li>
               </ul>
             </aside>
           </article>
           <article class="row" id="copyrightFooter">
-            <p class="h6 text-center">&copy; 2023. Todos los derechos reservados.</p>
+            <p class="h6 text-center text-light m-2">&copy; 2023. Todos los derechos reservados.</p>
           </article>
         `;
     footer[0].appendChild(section);
